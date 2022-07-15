1fe446e66cf7a3e1befea2df7defcbc61e3708fe
Bruno Zampieri <brunozampieri1985@gmail.com>
2022-07-15 16:23:44 -0300

    fix: remove typing animation

--- src/components/sections/Home/Home.tsx
+++ src/components/sections/Home/Home.tsx
@@ -3,33 +3,11 @@ import data from './Home.data'
 import { LangType, useLang } from '@context/LangContext'
 import Section from '@components/common/Section'
 import MatrixBackground from '@components/common/MatrixBackground'
-import { useEffect, useRef, useState } from 'react'
 import SocialMedia from '@components/common/SocialMedia'
 
 const Home: React.FC = () => {
    const { lang } = useLang()
-   const [isTyped, setIsTyped] = useState(false)
-
-   const paragraphRef = useRef<HTMLParagraphElement>(null)
-
-   useEffect(() => {
-      if (paragraphRef.current) {
-         if (!isTyped) {
-            let index = 0
-            let text = data.whoami[lang as LangType]
-            const interval = setInterval(() => {
-               if (index < text.length) {
-                  paragraphRef.current!.innerHTML += text[index]
-                  index++
-               } else {
-                  setIsTyped(true)
-                  clearInterval(interval)
-               }
-            }, 100)
-         }
-      }
-   }, [isTyped])
-
+ 
    return (
       <Section label="Home">
          <MatrixBackground />
@@ -40,7 +18,7 @@ const Home: React.FC = () => {
             <div className="home-greetings">
                <h1>{data.greetings[lang as LangType]}</h1>
             </div>
-            <p className="home-message" ref={paragraphRef}></p>
+            <p className="home-message" >{data.whoami[lang as LangType]}</p>
             <div className="home-social">
                <SocialMedia />
             </div>
